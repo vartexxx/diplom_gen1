d9015604059f66054ba4e59ce427318504ab0f73
Владислав Бурлака <vartexxx29@yandex.ru>
2022-03-04 01:35:36 +0500

    Last_Try

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import math as m
 import sqlite3 as sq
 
+import matplotlib.pyplot as plt
+import numpy as np
+
 
 class Debit:
     def __init__(self,
@@ -28,7 +31,7 @@ class Debit:
         return abs(q0 - my_q)
 
 
-class Filtr:  # Класс для обработки входных параметров и расчёта сводной таблицы
+class Filtr:  # Класс для обработки входных параметров и расчёта сводной таблиц
     def __init__(self,
                  dp: float,
                  q: float) -> None:
@@ -56,9 +59,31 @@ class Svodnaya:
         return self.koef_x * self.koef_y
 
 
-class Grpahics:  # Класс для рисования графиков функции
-    def __init__(self) -> None:
-        pass
+class Graphics:  # Класс для рисования графиков функции
+    def __init__(self,
+                 dp_2_default: float,
+                 dp_2: float,
+                 q: float) -> None:
+        self.dp_2_default = dp_2_default
+        self.dp_2 = dp_2
+        self.q = q
+
+    def plot_lib(self):
+        x = np.array(self.q)
+        y1 = np.array(self.dp_2_default)
+        y2 = np.array(self.dp_2)
+        fig = plt.figure(figsize=(7, 4))
+        fig.suptitle('Графики зависимости dP^2 от Q '
+                     'и графика dP^2/Q от Q')
+        plt.subplot(1, 2, 1)
+        plt.plot(x, y1, color='g', label='Зависимость dP^2/Q от Q')
+        plt.legend()
+        plt.grid()
+        plt.subplot(1, 2, 2)
+        plt.plot(x, y2, color='b', label='Зависимость dP^2 от Q')
+        plt.legend()
+        plt.grid()
+        return plt.show()
 
 
 with sq.connect('dikt.db') as con1:
@@ -70,27 +95,28 @@ with sq.connect('dikt.db') as con1:
             raise TypeError('Ошибка чтения базы данных')
         return data
     dq_equal = []  # Список значений погрешностей
-    #cur.execute("ALTER TABLE well_12102 ADD COLUMN IF NOT EXISTS 'dQ' 'float'")  # Создание столбца dQ в таблице well_12102
+    cur.execute("""ALTER TABLE well_12102 ADD COLUMN IF NOT EXISTS
+                'dQ' 'float'""")  # Создание столбца dQ в таблице well_12102
     sqlite_insert_query = """INSERT INTO well_12102
                              (dQ) VALUES (?)"""
     for result in cur:
         qd = read_package(result)
         c, p, y, t, z, q = qd  # Распаковка кортежа
         well = Debit(c, p, y, t, z, q)  # С оздание объекта well класса Debit
-        dq_equal.append(well.dq_equal())  # Создание списка погрешностей (ещё нужно отсортировать)
-    #cur.executemany(sqlite_insert_query, dq_equal)
-    #cur.execute("ALTER TABLE well_12102 ADD COLUMN 'de' 'float'")  # Создание столбца максимальной погрешности
-    dq_last = (sorted(dq_equal)[-1])  # Итоговое значение наибольшей погрешности
+        dq_equal.append(well.dq_equal())  # Создание списка погрешностей
+    cur.executemany(sqlite_insert_query, dq_equal)
+    cur.execute("ALTER TABLE well_12102 ADD COLUMN 'de' 'float'")
+    dq_last = (sorted(dq_equal)[-1])  # Значение наибольшей погрешност
     sqlite_insert_dq = """INSERT INTO WELL_12102
                           (de) VALUES (?)"""
-    #cur.executemany(sqlite_insert_dq, dq_last)  # Добавление значений максимальной погрешности в столбец de
+    cur.executemany(sqlite_insert_dq, dq_last)
 
 
 with sq.connect('filtr_koef.db') as con2:
     cur = con2.cursor()
     cur.execute('SELECT dP, Q FROM well_10201')
 
-    def read_package(data: list) -> Filtr:
+    def read_package(data: list) -> None:
         if data is None:
             raise TypeError('Ошибка чтения базы данных')
         return data
@@ -116,5 +142,35 @@ with sq.connect('filtr_koef.db') as con2:
     sum_x_y = sum(k_x_y)
     spot = (sum_x_y, sum_x_2, sum_x, sum_y)
     l_svodnaya = list(zip(k_x_y, k_x_2, koef_x, koef_y))
-    #cur.execute("INSERT INTO svodnaya_summ VALUES(?, ?, ?, ?)", spot)  # Кортеж суммы в SQL
-    #cur.executemany("INSERT INTO svodnaya VALUES(?, ?, ?, ?)", l_svodnaya)  # Распаковка архива кортежа из списков в SQL
+    cur.execute("INSERT INTO svodnaya_summ VALUES(?, ?, ?, ?)", spot)
+    cur.executemany("INSERT INTO svodnaya VALUES(?, ?, ?, ?)", l_svodnaya)
+    N = 7
+    koef_a_up = sum_y * sum_x_2 - sum_x * sum_x_y
+    koef_a_b_down = N * sum_x_2 - sum_x ** 2
+    koef_a = koef_a_up / koef_a_b_down
+    koef_b_up = N * sum_x_y - sum_x * sum_y
+    koef_b = koef_b_up / koef_a_b_down
+    cur.execute('SELECT A, B FROM well_10201')
+    for koef_ab in cur:
+        a_b = read_package(koef_ab)
+        a, b = a_b
+    pogr_a = (koef_a - a) / a
+    pogr_b = (koef_b - b) / b
+    cur.execute('SELECT Ppl, Pzab, Q FROM well_10201')
+    l_ppl = []
+    l_q_def = []
+    l_ppl_q_default = []
+    for res in cur:
+        ppl, pzab, q = res
+        dp_q = ((ppl * ppl - pzab * pzab)**2) / q
+        dp = ((ppl * ppl - pzab * pzab)**2)
+        l_ppl.append(dp)
+        l_q_def.append(q)
+        l_ppl_q_default.append(dp_q)
+    well_graph = Graphics(l_ppl_q_default, l_ppl, l_q_def)
+    well_graph.plot_lib()
+    cur.execute("""CREATE TABLE IF NOT EXIST KOEF_A_B (
+                koef_a FLOAT,
+                koef_b FLOAT
+                )""")
+    cur.execute("INSERT INTO KOEF_A_B VALUES(?, ?), koef_a, koef_b")
